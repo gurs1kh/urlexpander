a535ece7cd95d639df8bc8bf39e998c423298634
Manvir Singh <gurs1kh@live.com>
2015-08-09 19:29:00 +0000

    styles added

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,4 +4,13 @@ from .models import Entry
 class UrlForm(forms.ModelForm):
 	class Meta:
 		model = Entry
-		fields= ('short_url',)
+		fields = ('short_url',)
+		widgets = {
+			'short_url': forms.TextInput(attrs={
+				'type':'text',
+				'id':'url-input',
+				'name':'url-input',
+				'placeholder':'http://...',
+				'class':'form-control input-md',
+			}),
+		}
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Entry(models.Model):
 	short_url = models.CharField(max_length=2083)
 	long_url = models.CharField(max_length=2083)
