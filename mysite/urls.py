ca87fe3a48e5bb144e0965e6cdc91ed00541a9f3
Manvir Singh <gurs1kh@live.com>
2015-08-13 02:52:47 +0000

    added authentication requirement on main page

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('myapp.urls')),
+	# Examples:
+	# url(r'^$', 'mysite.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
+	
+	url(r'^admin', include(admin.site.urls)),
+	url(r'^login', auth_views.login),
+	url(r'^', include('myapp.urls')),
 ]
